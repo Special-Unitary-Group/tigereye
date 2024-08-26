1e236b2c58c69f360f4658a23b1ed9aead1648c3
Pablo González <jp_glzf@hotmail.com>
2024-08-26 14:14:11 -0600

    Call city names function

--- cities.py
+++ cities.py
@@ -6,6 +6,14 @@ class Cities:
     def __init__(self, grouped_df):
         self.grouped_df = grouped_df
 
+    def get_city_data(self, city_name):
+        if city_name not in self.grouped_df["City"].unique():
+            raise ValueError(f"City '{city_name}' not found in the dataset.")
+        city_df = self.grouped_df[self.grouped_df["City"] == city_name]
+        city_df = city_df.drop(["City"], axis=1)
+        return city_df
+
+
     def apodaca(self):
         df_apodaca = self.grouped_df[self.grouped_df["City"] == "Apodaca"]
         df_apodaca = df_apodaca.drop(["City"], axis=1)
